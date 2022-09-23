8b5b88da0412b21de4b7d0ad12c98c0c3ddba1f7
MitchelEidegit config --global user.email mitchmneide@gmail.com <mitchmneide@gmail.com>
2022-09-23 18:45:12 -0500

    took from end lesson snapshot code broke somehow

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,18 +1,18 @@
-import React, {useEffect} from 'react';
-import { useQuery } from '@apollo/client';
-import { useStoreContext } from '../../utils/GlobalState';
+import React, { useEffect } from 'react';
 import ProductItem from '../ProductItem';
-import { QUERY_PRODUCTS } from '../../utils/queries';
+import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
+import { useQuery } from '@apollo/client';
+import { QUERY_PRODUCTS } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
 
 function ProductList() {
-
   const [state, dispatch] = useStoreContext();
-  const {currentCategory} = state;
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
 
- 
+  const { currentCategory } = state;
+
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
     if (data) {
@@ -20,15 +20,27 @@ function ProductList() {
         type: UPDATE_PRODUCTS,
         products: data.products,
       });
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+    } else if (!loading) {
+      idbPromise('products', 'get').then((products) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products,
+        });
+      });
     }
-  }, [data, dispatch]);
-  
+  }, [data, loading, dispatch]);
+
   function filterProducts() {
     if (!currentCategory) {
       return state.products;
     }
-  
-    return state.products.filter(product => product.category._id === currentCategory);
+
+    return state.products.filter(
+      (product) => product.category._id === currentCategory
+    );
   }
 
   return (
--- server/server.js
+++ server/server.js
@@ -14,8 +14,6 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
-// server.applyMiddleware({ app });
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -26,18 +24,22 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-const startApolloServer= async (typeDefs, resolvers) => {
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
-  server.applyMiddleware({app})
+  server.applyMiddleware({ app });
+  
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    });
-  });
-}
-startApolloServer(typeDefs, resolvers);
-
+    })
+  })
+  };
+  
+  // Call the async function to start the server
+  startApolloServer(typeDefs, resolvers);
